34ad0594d979ad1bb3da32ee20f17f6454f443ed
Aymane Kenbouch <aymanekenbouch@gmail.com>
2025-10-30 00:40:11 +0100

    final version all manifests

--- Kubernetes/createscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl apply -f volume.yml
-kubectl apply -f secret.yml
-kubectl apply -f deployment.yml
-kubectl apply -f services.yml
-
--- Kubernetes/deletescript.sh
+++
@@ -1,8 +0,0 @@
-
-
-kubectl delete -f services.yml
-kubectl delete -f deployment.yml
-kubectl delete -f secret.yml
-kubectl delete -f volume.yml
-
-
